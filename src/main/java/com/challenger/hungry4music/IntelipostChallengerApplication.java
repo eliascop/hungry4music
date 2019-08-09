1b24dea0c7be8416c86b974ce44063942f823a90
Elias Oliveira <eliascop@yahoo.com.br>
2019-08-08 23:35:51 -0300

    kick-start

---
+++ src/main/java/com/challenger/hungry4music/IntelipostChallengerApplication.java
@@ -0,0 +1,13 @@
+package com.challenger.hungry4music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelipostChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelipostChallengerApplication.class, args);
+	}
+
+}
